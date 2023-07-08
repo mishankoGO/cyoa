b617ae24ae3566ee1b6c75378a234c86c87dee21
mishankoGO <arbuzofffffff@yandex.ru>
2023-07-08 14:33:52 +0900

    moved style.css

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	cli2 "github.com/mishankoGO/cyoa/internal/cli"
+	"github.com/mishankoGO/cyoa/internal/controllers"
 	"github.com/mishankoGO/cyoa/internal/storyteller"
 	"log"
+	"net/http"
 )
 
 func main() {
@@ -14,18 +15,18 @@ func main() {
 		log.Fatal(err)
 	}
 
-	//controller := controllers.NewController(storyTeller)
-	//router := controller.Route()
-	//
-	//server := &http.Server{
-	//	Addr:    ":8080",
-	//	Handler: router,
-	//}
-	//log.Println("Listening...")
-	//server.ListenAndServe()
-	cli := cli2.NewCli(storyTeller)
-	err = cli.Game()
-	if err != nil {
-		log.Fatal(err)
+	controller := controllers.NewController(storyTeller)
+	router := controller.Route()
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
 	}
+	log.Println("Listening...")
+	server.ListenAndServe()
+	//cli := cli2.NewCli(storyTeller)
+	//err = cli.Game()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 }
